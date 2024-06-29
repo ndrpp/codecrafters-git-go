b1c76d5e6d77ed98579398bf93bf7962226cd84b
ndrpp <popandrei230@yahoo.com>
2024-06-29 20:52:55 +0300

    Fix ls-tree, add --name-only flag support

--- cmd/mygit/main.go
+++ cmd/mygit/main.go
@@ -49,7 +49,7 @@ func main() {
 		hashObject(os.Args[2], os.Args[3])
 
 	case "ls-tree":
-		if os.Args[3] != "" {
+		if len(os.Args) == 4 {
 			listTree(os.Args[2], os.Args[3])
 		} else {
 			listTree("unflagged", os.Args[2])
@@ -61,31 +61,43 @@ func main() {
 	}
 }
 
-func listTree(flag, sha string) {
-	switch flag {
-	case "--name-only":
-		p, err := zlibDecompress(fmt.Sprintf(".git/objects/%s/%s", sha[0:2], sha[2:]))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "Failed to decompress file: %s\n", err)
-		}
+func parseTree(sha string) ([][]string, int) {
+	p, err := zlibDecompress(fmt.Sprintf(".git/objects/%s/%s", sha[0:2], sha[2:]))
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Failed to decompress file: %s\n", err)
+	}
 
-		var result [][]string
-		arr := strings.SplitN(string(p), "\x00", 2)
-		entries := strings.Split(arr[1], "\x00")
-		for i := range entries {
-			if i == 0 {
-				result = append(result, strings.Split(entries[i], " "))
-			} else if i == len(entries)-1 {
-				result[len(result)-1] = append(result[i-1], hex.EncodeToString([]byte(entries[i])))
-			} else {
-				fmt.Println("entries[i]: ", entries[i])
-				sha := entries[i][:20]
-				result[i-1] = append(result[i-1], hex.EncodeToString([]byte(sha)))
+	var result [][]string
+	arr := strings.SplitN(string(p), "\x00", 2)
+	entries := strings.Split(arr[1], "\x00")
+	for i := range entries {
+		if i == 0 {
+			result = append(result, strings.Split(entries[i], " "))
+		} else if i == len(entries)-1 {
+			result[len(result)-1] = append(result[i-1], hex.EncodeToString([]byte(entries[i])))
+		} else {
+			if entries[i] != "" {
+				hash := entries[i][:20]
+				result[i-1] = append(result[i-1], hex.EncodeToString([]byte(hash)))
 				result = append(result, []string{entries[i][20:26], entries[i][26:]})
 			}
 		}
+	}
 
-		for i := 0; i < len(entries)-1; i++ {
+	return result, len(entries)
+}
+
+func listTree(flag, sha string) {
+	switch flag {
+	case "unflagged":
+		result, length := parseTree(sha)
+		for i := 0; i < length-1; i++ {
+			fmt.Println(fmt.Sprintf("%s %s %s", strings.TrimSpace(result[i][0]), strings.TrimSpace(result[i][2]), strings.TrimSpace(result[i][1])))
+		}
+
+	case "--name-only":
+		result, length := parseTree(sha)
+		for i := 0; i < length-1; i++ {
 			fmt.Println(fmt.Sprintf("%s", strings.TrimSpace(result[i][1])))
 		}
 
